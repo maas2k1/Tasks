a08edcf548393c3acc17caf1bc733700a73e3e95
maas2k1 <102025817+maas2k1@users.noreply.github.com>
2023-06-19 01:34:40 +0500

    Create formscreen.test.js

---
+++ tests/formscreen.test.js
@@ -0,0 +1,21 @@
+import { handleSubmitForm } from './formscreen';
+
+
+const navigationMock = {
+  navigate: jest.fn(),
+};
+
+const alertMock = jest.fn();
+
+describe('Form Submission Feature', () => {
+  test('should navigate and show an alert message on form submission', () => {
+    const name = 'Ahmed';
+    const age = 22;
+
+    
+    handleSubmitForm(name, age, navigationMock, alertMock);
+
+    expect(navigationMock.navigate).toHaveBeenCalledWith('Home');
+    expect(alertMock).toHaveBeenCalledWith(`Welcome, ${name}! Your age is ${age}.`);
+  });
+});
